63b7fe182d45ce1788a2e850fab37998db857ad9
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2024-09-01 18:59:29 -0600

    init: initial project setup and deployment configuration
    - set up project structure
    - add initial deployment scripts and configuration files
    
    BREAKING CHANGE: this commit introduces the initial setup for the deployment pipeline.

---
+++ src/test/java/com/impact/brain/ImpactBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactBrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
