f88686ff98843fbb715d500b1aede07ab3c9a743
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2024-09-05 11:44:34 -0600

    chore: add asynchronous processing configuration
    - Created `AsyncConfig` class to enable asynchronous method execution in the Spring application.
    - Added `@Configuration` and `@EnableAsync` annotations to support async processing with CGLIB proxies.
    - Updated project to allow for efficient handling of asynchronous tasks.
    
    BREAKING CHANGE: Introduces asynchronous processing capabilities, requiring adjustments in method execution strategies.

---
+++ src/main/java/com/impact/brain/configuration/emailConfig/AsyncConfig.java
@@ -0,0 +1,22 @@
+package com.impact.brain.configuration.emailConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Isaac F. B. C.
+ * @since 9/5/2024 - 11:42 AM
+ */
+
+/**
+ * Configuration class for enabling asynchronous processing in the Spring application.
+ * <p>
+ * This class is marked with the {@link @Configuration} annotation to indicate that it contains
+ * Spring configuration. The {@link @EnableAsync} annotation enables Spring's asynchronous method
+ * execution capability. The attribute {@code proxyTargetClass = true} ensures that CGLIB proxies
+ * are used for classes, which allows for the creation of proxies for classes, not just interfaces.
+ * </p>
+ */
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfig {
+}
