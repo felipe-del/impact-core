af73a236a8cf30139181b63c462c12107b7cfdcf
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-09-11 23:59:00 -0600

    feat (common-space-module-implementation): The following classes have been added to the project SpaceController, SpaceDTO, SpaceEquipmentRepository, SpaceRepository, SpaceService, SpaceStatusRepository, and SpaceTypeRepository. All of them have been placed inside of the com.impact.brain.commonSpace package. Implemented getMappings for each of the entities inside of com.impact.brain.commonSpace.entity. Create and post mappings still pending.
    Signed-off-by: JoF00L <131736806+JoF00L@users.noreply.github.com>

---
+++ src/main/java/com/impact/brain/commonSpace/dto/SpaceDTO.java
@@ -0,0 +1,11 @@
+package com.impact.brain.commonSpace.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class SpaceDTO {
+
+
+}
---
+++ src/main/java/com/impact/brain/commonSpace/repository/SpaceRepository.java
@@ -0,0 +1,7 @@
+package com.impact.brain.commonSpace.repository;
+
+import com.impact.brain.commonSpace.entity.Space;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpaceRepository extends CrudRepository<Space, Integer> {
+}
