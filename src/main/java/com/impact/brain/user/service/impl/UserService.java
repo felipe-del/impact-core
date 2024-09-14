6aa81425cc4db1a2bdb0dd730ffdae8ad3b7df72
marcoleandro9 <marcoleandrito@gmail.com>
2024-09-14 15:10:51 -0600

    refactor(user-service): improved saveUser function to perform necessary validations
    - Added validation for duplicate emails on impact_db before saving a user.
    - Added logic to search for default "inactive" state and "Teacher" role and set this atributes for the new user.
    - Added functionality to find Users by Administrator Role and use to send emails on saveUser.
    - Added logic to notify administrators when a new user is created.

--- src/main/java/com/impact/brain/user/service/impl/UserService.java
+++ src/main/java/com/impact/brain/user/service/impl/UserService.java
@@ -5,7 +5,11 @@ import com.impact.brain.email.service.impl.EmailSendService;
 import com.impact.brain.email.util.EmailServiceUtil;
 import com.impact.brain.user.dto.UserDTO;
 import com.impact.brain.user.entity.User;
+import com.impact.brain.user.entity.UserRole;
+import com.impact.brain.user.entity.UserState;
 import com.impact.brain.user.repository.UserRepository;
+import com.impact.brain.user.repository.UserRoleRepository;
+import com.impact.brain.user.repository.UserStateRepository;
 import com.impact.brain.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,38 +29,83 @@ public class UserService implements IUserService {
 
     private final UserRepository userRepository;
     private final EmailSendService emailService;
+    private final UserStateRepository userStateRepository;
+    private final UserRoleRepository userRoleRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
-    public UserService(UserRepository userRepository, EmailSendService emailService) {
+    public UserService(UserRepository userRepository,UserStateRepository userStateRepository,UserRoleRepository userRoleRepository, EmailSendService emailService) {
         this.userRepository = userRepository;
+        this.userStateRepository = userStateRepository;
+        this.userRoleRepository = userRoleRepository;
         this.emailService = emailService;
     }
 
     @Override
-    public List<User> findAll(){
+    public List<User> findAll() {
         return this.userRepository.findAll();
     }
 
     @Override
     public User saveUser(User user) throws Exception {
-        // Encriptar la contraseña con BCrypt
+
+        Optional<User> existingUser = Optional.ofNullable(userRepository.findByEmail(user.getEmail()));
+        if (existingUser.isPresent()) {
+            throw new Exception("Email ya registrado " + user.getEmail());
+        }
+
+        /** Encrypt password with BCrypt */
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
 
-        //user.setRole("TEACHER");
-        //user.setState((byte) 0);
+        /** Search state "inactive" on DB */
+        if (user.getState() == null) {
+            UserState userState = userStateRepository.findByName("inactive");
+            if (userState != null) {
+                user.setState(userState);
+            } else {
+                throw new Exception("Estado 'inactive' no encontrado en la base de datos");
+            }
+        }
+        /** Search rol "Teacher" on DB */
+        if(user.getRole() == null) {
+            UserRole userRole = userRoleRepository.findByName("Teacher");
+            if(userRole != null) {
+                user.setRole(userRole);
+            }else{
+                throw new Exception("Rol 'Teacher' no encontrado en la base de datos");
+            }
+        }
 
+        /** Save User */
         User savedUser = this.userRepository.save(user);
         savedUser.setPassword(null);
 
-        // Preparar y enviar correo de bienvenida
+        /** Prepare and send welcome email */
         SendRequest sendRequest = EmailServiceUtil.prepareWelcomeEmail(savedUser);
         emailService.sendMessage(sendRequest, true);
 
+        /** Prepare and send notification-new-user email */
+        List<SendRequest> sendRequests = EmailServiceUtil.prepareNotificationEmailToAdmin(findUserByAdministratorRole());
+        for (SendRequest sendRequest_1 : sendRequests) {
+            emailService.sendMessage(sendRequest_1, true);
+        }
 
         return savedUser;
     }
+
+    private List<User> findUserByAdministratorRole()throws Exception{
+        UserRole adminRole = userRoleRepository.findByName("Administrator");
+        if(adminRole == null) {
+            throw new Exception("No hay rol de administrador en la base de datos");
+        }
+        List<User> users = userRepository.findByRole(adminRole);
+        if (userRepository.findByRole(adminRole) == null) {
+            throw new Exception("No hay usuarios con el rol de administrador");
+        }
+        return users;
+    }
+
     @Override
     public User changePassword(int userId, String newPassword) {
         // Find the user by userId
