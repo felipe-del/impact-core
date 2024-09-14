420bd00bbecd0154bc90fdeda1687d54fb83a33c
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-09-14 17:02:11 -0600

    fix (user-service): java.util.Optional import was missing.
    Signed-off-by: JoF00L <131736806+JoF00L@users.noreply.github.com>

--- src/main/java/com/impact/brain/user/service/impl/UserService.java
+++ src/main/java/com/impact/brain/user/service/impl/UserService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
