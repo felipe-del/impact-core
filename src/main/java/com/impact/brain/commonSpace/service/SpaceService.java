b5e0c64eaa41d9cc9142dce20923b135aa078b3a
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-09-14 15:53:59 -0600

    feat (space-service): 2 new repositories added (buildingRepository, buildingLocationRespository). There are also 4 new methods that have been added. buildings(), buildingLocations(), locationsByBuilding(), locationsOfBuilding().
    Signed-off-by: JoF00L <131736806+JoF00L@users.noreply.github.com>

--- src/main/java/com/impact/brain/commonSpace/service/SpaceService.java
+++ src/main/java/com/impact/brain/commonSpace/service/SpaceService.java
@@ -1,15 +1,13 @@
 package com.impact.brain.commonSpace.service;
 
-import com.impact.brain.commonSpace.entity.Space;
-import com.impact.brain.commonSpace.entity.SpaceEquipment;
-import com.impact.brain.commonSpace.entity.SpaceStatus;
-import com.impact.brain.commonSpace.entity.SpaceType;
-import com.impact.brain.commonSpace.repository.SpaceEquipmentRepository;
-import com.impact.brain.commonSpace.repository.SpaceRepository;
-import com.impact.brain.commonSpace.repository.SpaceStatusRepository;
-import com.impact.brain.commonSpace.repository.SpaceTypeRepository;
+import com.impact.brain.commonSpace.dto.BuildingLocationDTO;
+import com.impact.brain.commonSpace.sortedData.LocationsOfBuildings;
+import com.impact.brain.commonSpace.entity.*;
+import com.impact.brain.commonSpace.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Optional;
 
 @org.springframework.stereotype.Service("spaceService")
@@ -26,12 +24,64 @@ public class SpaceService {
     @Autowired
     private SpaceStatusRepository spaceStatusRepository;
 
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    @Autowired
+    private BuildingLocationRepository buildingLocationRepository;
+
     /* FindAll() methods to retrieve the entire list of each object type respectively  */
     public Iterable<SpaceType> spaceTypes() { return spaceTypeRepository.findAll(); }
     public Iterable<SpaceEquipment> spaceEquipments() { return spaceEquipmentRepository.findAll(); }
     public Iterable<SpaceStatus> spaceStatuses() { return spaceStatusRepository.findAll(); }
-
+    public Iterable<Building> buildings() { return buildingRepository.findAll(); }
     public Iterable<Space> spaces() { return spaceRepository.findAll(); }
+
+    /* Method to show all building locations, the BuildingLocationDTO
+    *  is being used to avoid errors from LAZY type fetching in the
+    *  attribute 'building' inside of the BuildingLocation class.
+    */
+    public Iterable<BuildingLocationDTO> buildingLocations() {
+        ArrayList<BuildingLocationDTO> buildingLocations = new ArrayList<>();
+        for(BuildingLocation buildingLocation : buildingLocationRepository.findAll()) {
+            BuildingLocationDTO newLocation = new BuildingLocationDTO();
+            newLocation.setId(buildingLocation.getId());
+            newLocation.setBuildingId(buildingLocation.getBuilding().getId());
+            newLocation.setFloorId(buildingLocation.getFloor());
+            buildingLocations.add(newLocation);
+        }
+        return buildingLocations;
+    }
+
+    // Method to sort all locations by their corresponding buildings
+    public Iterable<LocationsOfBuildings> locationsByBuilding() {
+        ArrayList<LocationsOfBuildings> locationsOfBuildings = new ArrayList<>();
+        for(Building currentBuilding : buildingRepository.findAll()) {
+            LocationsOfBuildings locationsOfCurrBuilding = new LocationsOfBuildings();
+            locationsOfCurrBuilding.setBuilding(currentBuilding);
+
+            for(BuildingLocationDTO currentLocation : buildingLocations()) {
+                if(currentBuilding.getId() == currentLocation.getBuildingId()) {
+                    locationsOfCurrBuilding.getLocations().add(currentLocation);
+                }
+            }
+
+            locationsOfBuildings.add(locationsOfCurrBuilding);
+        }
+        return locationsOfBuildings;
+    }
+
+    // Method to get the locations that are related to a single building
+    public LocationsOfBuildings locationsOfBuilding(int buildingId){
+        LocationsOfBuildings locationsOfBuilding = new LocationsOfBuildings();
+        locationsOfBuilding.setBuilding(buildingRepository.findBuildingBy(buildingId));
+        for(BuildingLocationDTO currentLocation : buildingLocations()) {
+            if(buildingId == currentLocation.getBuildingId()) {
+                locationsOfBuilding.getLocations().add(currentLocation);
+            }
+        }
+        return locationsOfBuilding;
+    }
 
     /* Save methods for each of the classes that have relationships with the Space class */
     public void saveSpaceType(SpaceType spaceType) {
@@ -49,9 +99,20 @@ public class SpaceService {
         System.out.println("Saving spaceEquipment: " + spaceEquipment);
     }
 
-    /* Methods to get objetcts */
+    /* Methods to get objects */
     public Optional<SpaceType> spaceTypeById(int id) {
         return spaceTypeRepository.findById(id);
     }
 
+    public Optional<SpaceStatus> spaceStatusById(int id) {
+        return spaceStatusRepository.findById(id);
+    }
+
+    public Optional<Building> buildingById(int id) {
+        return buildingRepository.findById(id);
+    }
+
+    public Optional<BuildingLocation> buildingLocationById(int id) {
+        return buildingLocationRepository.findById(id);
+    }
 }
