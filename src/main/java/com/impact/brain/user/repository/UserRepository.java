07b3f69e2b19f3ed63aeca9eea70224e7b0ec20e
marcoleandro9 <marcoleandrito@gmail.com>
2024-09-14 15:34:25 -0600

    refactor(method): Added method findByRole using logic from JpaRepository
    - Added to find users by roles

--- src/main/java/com/impact/brain/user/repository/UserRepository.java
+++ src/main/java/com/impact/brain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.impact.brain.user.repository;
 
 import com.impact.brain.user.entity.User;
+import com.impact.brain.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,5 +19,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("SELECT User FROM User u")
     List<User> findAll();
+
+    List<User> findByRole(UserRole role);
 
 }
