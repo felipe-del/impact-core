2e88bc09267bb15e746750e5af30174f4be43efe
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-09-29 17:01:44 -0600

    feat (space-repository): added a query to find spaces by id.

--- src/main/java/com/impact/brain/commonSpace/repository/SpaceRepository.java
+++ src/main/java/com/impact/brain/commonSpace/repository/SpaceRepository.java
@@ -1,7 +1,10 @@
 package com.impact.brain.commonSpace.repository;
 
 import com.impact.brain.commonSpace.entity.Space;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpaceRepository extends CrudRepository<Space, Integer> {
+    @Query("SELECT sp FROM Space sp WHERE sp.id = ?1")
+    Space findSpaceById(int id);
 }
