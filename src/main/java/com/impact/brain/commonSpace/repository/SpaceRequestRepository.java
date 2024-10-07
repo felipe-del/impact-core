59ed5fef26e58eed1d524306008af04d128fa6b2
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-10-07 00:14:34 -0600

    feat (space-request/reservation-repository): added both repos.

---
+++ src/main/java/com/impact/brain/commonSpace/repository/SpaceRequestRepository.java
@@ -0,0 +1,7 @@
+package com.impact.brain.commonSpace.repository;
+
+import com.impact.brain.commonSpace.entity.SpaceRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpaceRequestRepository extends JpaRepository<SpaceRequest, Integer> {
+}
