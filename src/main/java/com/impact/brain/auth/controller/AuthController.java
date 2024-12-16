996a9162dd12c302750fe54028c538dda934f634
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-16 16:51:50 -0600

    Update AuthController to support login and logout endpoints
    Refactored the AuthController to include new login and logout functionalities. Introduced services for authentication, user management, and JWT handling to streamline token generation and blacklisting. Updated the endpoint path to "/api/auth" for consistency.

--- src/main/java/com/impact/brain/auth/controller/AuthController.java
+++ src/main/java/com/impact/brain/auth/controller/AuthController.java
@@ -1,23 +1,34 @@
 package com.impact.brain.auth.controller;
 
 import com.impact.brain.auth.dto.LoginRequest;
+import com.impact.brain.auth.dto.LogoutRequest;
+import com.impact.brain.auth.service.AuthServiceImpl;
+import com.impact.brain.auth.service.JwtServiceImpl;
 import com.impact.brain.user.entity.User;
-import com.impact.brain.user.service.UserService;
-import com.impact.brain.user.service.UserServiceImpl;
+import com.impact.brain.user.service.user.UserServiceImpl;
+import com.impact.brain.user.service.userJwtToken.UserJwtTokenServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final UserServiceImpl userServiceImpl;
+    private final AuthServiceImpl authService;
+    private final UserServiceImpl userService;
+    private final JwtServiceImpl jwtService;
 
-    @GetMapping
-    public String auth(@RequestBody LoginRequest request) {
-        return userServiceImpl.findByEmail(request.getUsername()).getName();
+    @PostMapping("/login")
+    public String auth(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest httpServletRequest) {
+        User user = userService.findByEmail(loginRequest.getEmail());
+        return jwtService.generateToken(user);
     }
+
+    @PostMapping("/logout")
+    public void logout(@RequestBody LogoutRequest logoutRequest) {
+        jwtService.blacklistToken(logoutRequest.getJwtToken());
+    }
+
 }
