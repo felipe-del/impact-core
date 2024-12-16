2c8385db3ca091926f25283c5bc68b328608ffd6
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-15 20:21:44 -0600

    Add CORS configuration for allowing cross-origin requests
    Introduce `WebConfig` implementing `WebMvcConfigurer` to enable CORS for the application. This setup allows requests from `http://localhost:5173` with specified HTTP methods to support frontend-backend interaction.

---
+++ src/main/java/com/impact/brain/configuration/security/WebConfig.java
@@ -0,0 +1,38 @@
+package com.impact.brain.configuration.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for setting up CORS (Cross-Origin Resource Sharing) in the application.
+ * <p>
+ * This class implements the {@link WebMvcConfigurer} interface to provide custom configuration for
+ * CORS. It allows cross-origin requests from specified origins and defines which HTTP methods are
+ * permitted.
+ * </p>
+ *
+ * @author Isaac F. B. C.
+ * @since 9/5/2024 - 11:53 AM
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Configures CORS mappings for the application.
+     * <p>
+     * This method defines the CORS settings for the application, specifying which origins and HTTP
+     * methods are allowed for cross-origin requests. It ensures that the frontend application running
+     * on a specific origin can interact with the backend server.
+     * </p>
+     *
+     * @param registry the {@link CorsRegistry} object used to configure CORS settings.
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
