10ee2e00a711653340bf599d5b6cf6e3467802a3
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 16:33:51 -0600

    Add AuthService with login and register functionality
    Implemented the AuthService class to handle user authentication and registration. Includes methods for generating JWT tokens upon login and validating user credentials. The registration process ensures unique emails and assigns roles based on the input.

---
+++ src/main/java/com/impact/core/module/auth/service/AuthService.java
@@ -0,0 +1,85 @@
+package com.impact.core.module.auth.service;
+
+import com.impact.core.module.auth.payload.request.LoginRequest;
+import com.impact.core.module.auth.payload.request.RegisterRequest;
+import com.impact.core.module.auth.payload.response.JwtResponse;
+import com.impact.core.module.auth.payload.response.MessageResponse;
+import com.impact.core.module.user.entity.ERole;
+import com.impact.core.module.user.entity.User;
+import com.impact.core.module.user.entity.UserRole;
+import com.impact.core.module.user.service.UserRoleService;
+import com.impact.core.module.user.service.UserService;
+import com.impact.core.security.jwt.JwtUtils;
+import com.impact.core.security.service.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("authService")
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final UserRoleService userRoleService;
+    private final PasswordEncoder encoder;
+    private final JwtUtils jwtUtils;
+
+    public JwtResponse login(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        return new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles);
+    }
+
+    public ResponseEntity<MessageResponse> register(RegisterRequest registerRequest) {
+        System.out.println("registerRequest = " + registerRequest);
+        if (userService.existsByEmail(registerRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: El email ya está en uso!"));
+        }
+
+        User user = User.builder()
+                .id(0)
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .password(encoder.encode(registerRequest.getPassword()))
+                .role(getUserRole(registerRequest.getRole()))
+                .state(null)
+                .build();
+
+        userService.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("Usuario registrado correctamente!"));
+    }
+
+    private UserRole getUserRole(String role) {
+        return switch (role.toLowerCase()) {
+            case "admin" -> userRoleService.findByName(ERole.ROLE_ADMINISTRATOR);
+            case "manager" -> userRoleService.findByName(ERole.ROLE_MANAGER);
+            default -> userRoleService.findByName(ERole.ROLE_TEACHER);
+        };
+    }
+
+}
