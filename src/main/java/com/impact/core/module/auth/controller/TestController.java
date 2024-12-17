1140a165cf9e46ea51f2ee0f136fd18c240a73a2
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 17:37:44 -0600

    Refactor HEADER and PREFIX variables into parseJwt method
    Moved HEADER and PREFIX into the parseJwt method to localize their usage. This improves code readability by limiting their scope to where they are needed, reducing clutter in the class.

--- src/main/java/com/impact/core/module/auth/controller/TestController.java
+++ src/main/java/com/impact/core/module/auth/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.impact.core.module.auth.controller;
 
+import com.impact.core.module.user.entity.UserRole;
+import com.impact.core.module.user.service.UserRoleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final UserRoleService userRoleService;
+
     @GetMapping("/all")
-    public String allAccess() {
-        return "Public Content.";
+    public List<UserRole> allAccess() {
+        return userRoleService.findAll();
     }
 
     @GetMapping("/manager-admin")
