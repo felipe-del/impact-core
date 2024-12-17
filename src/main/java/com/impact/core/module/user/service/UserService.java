cd8b482b77909ee6cdfc6866d7fb899455b11d85
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 16:57:53 -0600

    Add UserService implementation for user management
    Introduces a new `UserService` class to handle user-related operations. Provides methods to find, check existence, and save users using `UserRepository`. This enhances separation of concerns and prepares the module for future scalability.

---
+++ src/main/java/com/impact/core/module/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.impact.core.module.user.service;
+
+import com.impact.core.module.user.entity.User;
+import com.impact.core.module.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("No existe usuario con email: " + email));
+    }
+
+    public Boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
