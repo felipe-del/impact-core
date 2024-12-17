4205b3390e87ebcc8625f0d711bd60a6a0a485bd
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 16:54:44 -0600

    Refactor User entity and relocate to new package
    Renamed `User` entity to a new package, updated its class annotations, and adjusted imports for consistency. Added `@Builder`, `@NoArgsConstructor`, `@AllArgsConstructor`, and `@JsonIgnore` for improved utility and security. Simplified references to `UserRole` and `UserState` by importing them directly.

--- src/main/java/com/impact/core/module/user/entity/User.java
+++ src/main/java/com/impact/core/module/user/entity/User.java
@@ -1,18 +1,23 @@
-package com.impact.core.user.entity;
+package com.impact.core.module.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.impact.core.entities.UserState;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Size(max = 100)
@@ -28,14 +33,15 @@ public class User {
     @Size(max = 100)
     @NotNull
     @Column(name = "password", nullable = false, length = 100)
+    @JsonIgnore
     private String password;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id")
-    private com.impact.core.entities.UserRole role;
+    private UserRole role;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "state_id")
-    private com.impact.core.entities.UserState state;
+    private UserState state;
 
 }
