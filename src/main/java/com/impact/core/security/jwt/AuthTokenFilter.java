b35474d8ccac42d8ac87c0528bdadc9b27ecde60
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 16:34:11 -0600

    Add AuthTokenFilter for JWT validation in request handling
    This commit introduces the AuthTokenFilter, a servlet filter that validates JWT tokens in requests. It extracts the token, verifies it, and sets up user authentication in the SecurityContext. This is essential for securing endpoints and managing user sessions.

---
+++ src/main/java/com/impact/core/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,76 @@
+package com.impact.core.security.jwt;
+
+import com.impact.core.security.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    private final String HEADER = "Authorization";
+
+    private final String PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+                System.out.println("Username: " + username);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader(HEADER);
+
+        if (headerAuth == null) {
+            logger.warn("Authorization header is missing");
+            return null;
+        }
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith(PREFIX)) {
+            return headerAuth.substring(7);
+        }
+
+        logger.warn("JWT token is missing or does not start with Bearer");
+        return null;
+    }
+}
