babc2cbcf5f11c813e3d61a3da2a98b7b2c020b8
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-17 17:38:10 -0600

    Add method to fetch all user roles in UserRoleRepository
    Added a `findAll` method to the UserRoleRepository interface, enabling retrieval of all user roles. This expands functionality for accessing role data and supports future use cases requiring comprehensive role lists.

--- src/main/java/com/impact/core/module/user/repository/UserRoleRepository.java
+++ src/main/java/com/impact/core/module/user/repository/UserRoleRepository.java
@@ -5,9 +5,12 @@ import com.impact.core.module.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     Optional<UserRole> findByName(ERole name);
+
+    List<UserRole> findAll();
 }
