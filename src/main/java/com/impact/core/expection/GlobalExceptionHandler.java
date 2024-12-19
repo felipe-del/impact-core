413f0b6de50a57d68a70fb898d10f1ae4ff55e33
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-19 14:40:32 -0600

    Add handlers for ResourceNotFound and Conflict exceptions
    Introduced specific exception handlers in GlobalExceptionHandler for ResourceNotFoundException and ConflictException to standardize error responses. Added a helper method to centralize response creation and ensure consistent formatting. This enhances API error handling and improves client feedback.

--- src/main/java/com/impact/core/expection/GlobalExceptionHandler.java
+++ src/main/java/com/impact/core/expection/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.impact.core.expection;
 
+import com.impact.core.expection.customException.ConflictException;
+import com.impact.core.expection.customException.ResourceNotFoundException;
+import com.impact.core.expection.payload.ErrorData;
 import com.impact.core.util.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +11,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,5 +34,42 @@ public class GlobalExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<ErrorData>> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request) {
+        return handleException(HttpStatus.NOT_FOUND, ex.getMessage(), "Recurso no encontrado", request);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ApiResponse<ErrorData>> handleConflict(ConflictException ex, WebRequest request) {
+        return handleException(HttpStatus.CONFLICT, ex.getMessage(), "Conflicto", request);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private ResponseEntity<ApiResponse<ErrorData>> handleException(HttpStatus httpStatus, String message, String userMessage, WebRequest request) {
+        String path = getPath(request);
+
+        // Build the error data
+        ErrorData errorData = ErrorData.builder()
+                .status(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
+                .message(message)
+                .path(path)
+                .build();
+
+        // Build the response
+        ApiResponse<ErrorData> response = ApiResponse.<ErrorData>builder()
+                .message(userMessage)
+                .data(errorData)
+                .build();
+
+        // Return the response
+        return new ResponseEntity<>(response, httpStatus);
+    }
+
+    private String getPath(WebRequest request) {
+        return request.getDescription(false);
     }
 }
