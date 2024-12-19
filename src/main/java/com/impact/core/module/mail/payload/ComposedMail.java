ea6ee40e24793312e6c4e0ea540422746fe563db
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-19 13:22:04 -0600

    Add ComposedMail class to represent email payload structure
    Introduced a new `ComposedMail` class with fields for recipient, subject, template, and metadata. This class utilizes Lombok annotations for cleaner code and supports payload handling in the mail module. It will simplify email construction and facilitate better modularity.

---
+++ src/main/java/com/impact/core/module/mail/payload/ComposedMail.java
@@ -0,0 +1,20 @@
+package com.impact.core.module.mail.payload;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComposedMail {
+    private String to;
+
+    private String subject;
+
+    private String template;
+
+    private List<MetaData> metaData;
+}
