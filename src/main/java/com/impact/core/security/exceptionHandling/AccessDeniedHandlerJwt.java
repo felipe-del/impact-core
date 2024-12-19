88c3584ec499aa1ec7c8be94f1e7616eda2626ad
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-19 14:38:21 -0600

    Rename welcome email template and adjust logo max width
    Renamed the email template file to improve clarity and consistency in naming. Updated the logo's max-width from 200px to 500px to ensure better visual display in the email footer.

---
+++ src/main/java/com/impact/core/security/exceptionHandling/AccessDeniedHandlerJwt.java
@@ -0,0 +1,47 @@
+package com.impact.core.security.exceptionHandling;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.impact.core.expection.payload.ErrorData;
+import com.impact.core.util.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandlerJwt implements AccessDeniedHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccessDeniedHandlerJwt.class);
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+
+        logger.error("Forbidden error: {}", accessDeniedException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ErrorData errorData = ErrorData.builder()
+                .status(HttpServletResponse.SC_FORBIDDEN)
+                .error("Forbidden")
+                .message("No tienes permisos para acceder a este recurso.")
+                .path(request.getServletPath())
+                .build();
+
+        ApiResponse<ErrorData> apiResponse = ApiResponse.<ErrorData>builder()
+                .message("Acceso prohibido")
+                .data(errorData)
+                .build();
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), apiResponse);
+    }
+}
