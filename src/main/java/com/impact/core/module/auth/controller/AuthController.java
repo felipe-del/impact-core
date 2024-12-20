4466f23e7fa0bd4104f0f8e8775bcaaaf4c87d5b
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-20 00:58:07 -0600

    ``` Refactor AuthController to enhance API response structure
    Updated login and register endpoints to wrap responses in a standardized ApiResponse format for consistency. Added a new getUserSession endpoint with pre-authorization to retrieve user session data securely. These changes improve response clarity and align with best practices.
    ```

--- src/main/java/com/impact/core/module/auth/controller/AuthController.java
+++ src/main/java/com/impact/core/module/auth/controller/AuthController.java
@@ -4,9 +4,13 @@ import com.impact.core.module.auth.payload.request.LoginRequest;
 import com.impact.core.module.auth.payload.request.RegisterRequest;
 import com.impact.core.module.auth.payload.response.JwtResponse;
 import com.impact.core.module.auth.service.AuthService;
+import com.impact.core.module.user.dto.UserDTO;
+import com.impact.core.module.user.entity.User;
+import com.impact.core.util.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +20,33 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-        return ResponseEntity.ok(authService.login(loginRequest));
+    public ResponseEntity<ApiResponse<JwtResponse>> login(@Valid @RequestBody LoginRequest loginRequest) {
+        JwtResponse jwtResponse = authService.login(loginRequest);
+
+        return ResponseEntity.ok(ApiResponse.<JwtResponse>builder()
+                .message("Inicio de sesión exitoso")
+                .data(jwtResponse)
+                .build());
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(authService.register(registerRequest));
+    public ResponseEntity<ApiResponse<UserDTO>> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        UserDTO userDTO = authService.register(registerRequest);
+
+        return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
+                .message("Usuario registrado exitosamente")
+                .data(userDTO)
+                .build());
+    }
+
+    @GetMapping("/getUserSession")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<UserDTO>> getUserSession() {
+        UserDTO dto = authService.getUserSession();
+
+        return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
+                .message("Usuario obtenido exitosamente")
+                .data(dto)
+                .build());
     }
 }
