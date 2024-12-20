5eacb483de4d4525fa1ed1e54fc8c404ea964228
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-20 01:07:49 -0600

    Update UserRoleService to use EUserRole and custom exception
    Replaced ERole with EUserRole in findByName method to align with updated enum structure. Switched to ResourceNotFoundException for more specific error handling when a role is not found.

--- src/main/java/com/impact/core/module/user/service/UserService.java
+++ src/main/java/com/impact/core/module/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.impact.core.module.user.service;
 
+import com.impact.core.module.auditLog.listener.AuditLogListener;
+import com.impact.core.module.user.dto.UserDTO;
 import com.impact.core.module.user.entity.User;
 import com.impact.core.module.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,4 +25,22 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    public User findById(Integer id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("No existe usuario con id: " + id));
+    }
+
+    // MAPPER METHODS
+
+    public UserDTO toDTO(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole().getName().toString())
+                .stateName(user.getState().getName().toString())
+                .build();
+    }
+
 }
