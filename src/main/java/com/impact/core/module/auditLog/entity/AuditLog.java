695cf68cf81648e9513e5385e05680c79de9d2c0
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-20 00:57:35 -0600

    Add AuditLog entity for tracking user actions
    Introduced the AuditLog entity to log actions performed on various entities. It includes fields for entityName, action, details, timestamp, and a relation to the User entity. This will support better accountability and traceability within the application.

---
+++ src/main/java/com/impact/core/module/auditLog/entity/AuditLog.java
@@ -0,0 +1,47 @@
+package com.impact.core.module.auditLog.entity;
+
+import com.impact.core.module.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "audit_log")
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "entity_name", nullable = false, length = 50)
+    private String entityName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "action", nullable = false, length = 50)
+    private String action;
+
+    @Lob
+    @Column(name = "details")
+    private String details;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "timestamp")
+    private Instant timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
