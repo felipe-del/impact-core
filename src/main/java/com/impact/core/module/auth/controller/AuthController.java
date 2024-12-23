3f26bcf4fac39161f79db7767c42700fa0cc7f15
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-23 01:48:05 -0600

    Add Logout endpoint and change sout text

--- src/main/java/com/impact/core/module/auth/controller/AuthController.java
+++ src/main/java/com/impact/core/module/auth/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.impact.core.module.auth.controller;
 
 import com.impact.core.module.auth.payload.request.LoginRequest;
+import com.impact.core.module.auth.payload.request.LogoutRequest;
 import com.impact.core.module.auth.payload.request.RegisterRequest;
 import com.impact.core.module.auth.payload.response.JwtResponse;
 import com.impact.core.module.auth.service.AuthService;
 import com.impact.core.module.user.dto.UserDTO;
-import com.impact.core.module.user.entity.User;
 import com.impact.core.util.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +24,7 @@ public class AuthController {
         JwtResponse jwtResponse = authService.login(loginRequest);
 
         return ResponseEntity.ok(ApiResponse.<JwtResponse>builder()
-                .message("Inicio de sesión exitoso")
+                .message("Usuario logueado.")
                 .data(jwtResponse)
                 .build());
     }
@@ -34,7 +34,7 @@ public class AuthController {
         UserDTO userDTO = authService.register(registerRequest);
 
         return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
-                .message("Usuario registrado exitosamente")
+                .message("Usuario registrado.")
                 .data(userDTO)
                 .build());
     }
@@ -45,8 +45,18 @@ public class AuthController {
         UserDTO dto = authService.getUserSession();
 
         return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
-                .message("Usuario obtenido exitosamente")
+                .message("Usuario de la sesión obtenido.")
                 .data(dto)
+                .build());
+    }
+
+    @GetMapping("/logout")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<Void>> logout(@Valid @RequestBody LogoutRequest logoutRequest) {
+        authService.logout(logoutRequest);
+
+        return ResponseEntity.ok(ApiResponse.<Void>builder()
+                .message("Usuario deslogueado.")
                 .build());
     }
 }
