369bdcde1b3ed0b1a809d0332aeae37ae710c46f
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:05:54 -0600

    Add UserTokenRepository to manage user token operations
    Introduce `UserTokenRepository` interface extending JpaRepository to handle database operations for `UserToken` entities. Includes methods to find a token and check for its existence, enabling efficient token management.

---
+++ src/main/java/com/impact/core/module/user/repository/UserTokenRepository.java
@@ -0,0 +1,14 @@
+package com.impact.core.module.user.repository;
+
+import com.impact.core.module.user.entity.UserToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserTokenRepository extends JpaRepository<UserToken, Long> {
+    Optional<UserToken> findByToken(String token);
+    boolean existsByToken(String token);
+}
