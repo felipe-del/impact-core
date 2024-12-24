1d13bfc69d34f9ba6d62699e340cf8adddcf9c31
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:05:14 -0600

    Refactor user lookup method in UserDetailsServiceImpl
    Replaced `findByEmail` with `findImpactUser` to improve clarity and better align with the method's intent. Removed unused `UserRepository` and `@RequiredArgsConstructor` imports to clean up the codebase. This enhances maintainability and reduces potential confusion.

--- src/main/java/com/impact/core/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/impact/core/security/service/UserDetailsServiceImpl.java
@@ -1,9 +1,7 @@
 package com.impact.core.security.service;
 
 import com.impact.core.module.user.entity.User;
-import com.impact.core.module.user.repository.UserRepository;
 import com.impact.core.module.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,7 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findByEmail(username);
+        User user = userService.findImpactUser(username);
 
         return UserDetailsImpl.build(user);
     }
