5e5346380c79fddbb93e242c8cd2e35507badd4a
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:05:44 -0600

    Rename and refactor UserToken entity with annotations
    Renamed the UserToken file to a new package for better organization. Added Lombok annotations like @Builder, @NoArgsConstructor, and @AllArgsConstructor for improved code efficiency and readability. Included @GeneratedValue annotation for id field to enable auto-generation.

--- src/main/java/com/impact/core/module/user/entity/UserToken.java
+++ src/main/java/com/impact/core/module/user/entity/UserToken.java
@@ -1,22 +1,24 @@
-package com.impact.core.entities;
+package com.impact.core.module.user.entity;
 
-import com.impact.core.module.user.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "user_token")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserToken {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
