7764ea36693bf3cb5937ac49415f05be0e3a731c
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:44:16 -0600

    Add AssetController to handle asset-related API requests
    Introduced a new controller with a base endpoint `/api/asset`. This serves as the entry point for future asset management features in the application.

---
+++ src/main/java/com/impact/core/module/asset/controller/AssetController.java
@@ -0,0 +1,12 @@
+package com.impact.core.module.asset.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/asset")
+@RequiredArgsConstructor
+public class AssetController {
+
+}
