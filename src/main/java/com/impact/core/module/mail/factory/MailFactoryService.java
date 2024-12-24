87c53c14862603eadb4605a2351008fd91c5a889
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:03:57 -0600

    Add method to generate forgot password email in MailFactoryService
    Introduced a new `createForgotPasswordEmail` method to compose emails for password reset functionality. It generates emails using user and token data while leveraging metadata and a predefined template to ensure consistent formatting.

--- src/main/java/com/impact/core/module/mail/factory/MailFactoryService.java
+++ src/main/java/com/impact/core/module/mail/factory/MailFactoryService.java
@@ -4,6 +4,7 @@ import com.impact.core.module.mail.payload.ComposedMail;
 import com.impact.core.module.mail.payload.MetaData;
 import com.impact.core.module.mail.enun.EMailTemplate;
 import com.impact.core.module.user.entity.User;
+import com.impact.core.module.user.entity.UserToken;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +19,26 @@ public class MailFactoryService {
                 .to(user.getEmail())
                 .subject("Bienvenido a IMPACT")
                 .template(EMailTemplate.WELCOME_EMAIL.getTemplateName())
+                .metaData(metaData)
+                .imageNames(List.of(
+                        "UCR_CIMPA_LOGO.png"
+                ))
+                .build();
+    }
+
+    public static ComposedMail createForgotPasswordEmail(UserToken userToken) {
+        User user = userToken.getUser();
+        String token = userToken.getToken();
+
+        List<MetaData> metaData = new ArrayList<>();
+
+        metaData.add(new MetaData("name", user.getName()));
+        metaData.add(new MetaData("token", token));
+
+        return ComposedMail.builder()
+                .to(user.getEmail())
+                .subject("Restablecer contraseña")
+                .template(EMailTemplate.FORGOT_PASSWORD_EMAIL.getTemplateName())
                 .metaData(metaData)
                 .imageNames(List.of(
                         "UCR_CIMPA_LOGO.png"
