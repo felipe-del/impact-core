a835359175f1fc56dd9f70f17c2a4c01e5dcbc35
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:00:54 -0600

    Add new endpoints for user management and password recovery
    Introduced endpoints for forgot password, reset password, change user state, and change user role to enhance functionality. Updated URL for the "user-session" endpoint for consistency. Refactored import statements for better code readability.

--- src/main/java/com/impact/core/module/auth/controller/AuthController.java
+++ src/main/java/com/impact/core/module/auth/controller/AuthController.java
@@ -1,8 +1,6 @@
 package com.impact.core.module.auth.controller;
 
-import com.impact.core.module.auth.payload.request.LoginRequest;
-import com.impact.core.module.auth.payload.request.LogoutRequest;
-import com.impact.core.module.auth.payload.request.RegisterRequest;
+import com.impact.core.module.auth.payload.request.*;
 import com.impact.core.module.auth.payload.response.JwtResponse;
 import com.impact.core.module.auth.service.AuthService;
 import com.impact.core.module.user.dto.UserDTO;
@@ -39,7 +37,7 @@ public class AuthController {
                 .build());
     }
 
-    @GetMapping("/getUserSession")
+    @GetMapping("/user-session")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<ApiResponse<UserDTO>> getUserSession() {
         UserDTO dto = authService.getUserSession();
@@ -57,6 +55,46 @@ public class AuthController {
 
         return ResponseEntity.ok(ApiResponse.<Void>builder()
                 .message("Usuario deslogueado.")
+                .build());
+    }
+
+    @GetMapping("/forgot-password")
+    public ResponseEntity<ApiResponse<Void>> forgotPassword(@RequestBody ForgotPasswordRequest forgotPasswordRequest) {
+        authService.forgotPassword(forgotPasswordRequest);
+
+        return ResponseEntity.ok(ApiResponse.<Void>builder()
+                .message("Se ha enviado un correo con el token de recuperación.")
+                .build());
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<ApiResponse<Void>> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        authService.resetPassword(resetPasswordRequest);
+
+        return ResponseEntity.ok(ApiResponse.<Void>builder()
+                .message("Contraseña restablecida.")
+                .build());
+    }
+
+    @PostMapping("/change-user-state")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public ResponseEntity<ApiResponse<UserDTO>> changeUserState(@Valid @RequestBody ChangeUserStateRequest changeUserStateRequest) {
+        UserDTO userDTO = authService.changeUserState(changeUserStateRequest);
+
+        return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
+                .message("Estado del usuario cambiado a '" + userDTO.getStateName() + "'.")
+                .data(userDTO)
+                .build());
+    }
+
+    @PostMapping("/change-user-role")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public ResponseEntity<ApiResponse<UserDTO>> changeUserRole(@Valid @RequestBody ChangeUserRoleRequest changeUserRoleRequest) {
+        UserDTO userDTO = authService.changeUserRole(changeUserRoleRequest);
+
+        return ResponseEntity.ok(ApiResponse.<UserDTO>builder()
+                .message("Rol del usuario cambiado a '" + userDTO.getRoleName() + "'.")
+                .data(userDTO)
                 .build());
     }
 }
