e4574eab7d1a4773f0640e9d54e2502cabc18c65
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-24 16:01:36 -0600

    Add FORGOT_PASSWORD_EMAIL to EMailTemplate enum
    Introduced a new enum constant, FORGOT_PASSWORD_EMAIL, to represent the forgot password email template. This change enhances the template management for email functionality by supporting password recovery scenarios.

--- src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
+++ src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
@@ -1,7 +1,8 @@
 package com.impact.core.module.mail.enun;
 
 public enum EMailTemplate {
-    WELCOME_EMAIL("new_user_welcome_email_template");
+    WELCOME_EMAIL("new_user_welcome_email_template"),
+    FORGOT_PASSWORD_EMAIL("forgot_password_email_template");
 
     private final String templateName;
 
