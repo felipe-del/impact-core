fdd620705a6523b82179fb2a38103470ba143c4c
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 03:47:46 -0600

    Add CRUD endpoints for SupplierController
    Implemented GET, PUT, and DELETE endpoints to manage suppliers. Updated POST endpoint to return a proper SupplierResponse DTO. These changes enhance supplier management and improve endpoint consistency.

--- src/main/java/com/impact/core/module/supplier/controller/SupplierController.java
+++ src/main/java/com/impact/core/module/supplier/controller/SupplierController.java
@@ -2,16 +2,17 @@ package com.impact.core.module.supplier.controller;
 
 import com.impact.core.module.supplier.payload.request.SupplierRequest;
 import com.impact.core.module.supplier.entity.Supplier;
+import com.impact.core.module.supplier.payload.response.SupplierResponse;
 import com.impact.core.module.supplier.service.SupplierService;
 import com.impact.core.util.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/supplier")
@@ -19,17 +20,54 @@ import org.springframework.web.bind.annotation.RestController;
 public class SupplierController {
     public final SupplierService supplierService;
 
-    @PostMapping
+    @GetMapping
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<ApiResponse<SupplierRequest>> saveSupplier(@Valid @RequestBody SupplierRequest supplierRequest) {
-        Supplier supplier = supplierService.save(supplierRequest);
-        SupplierRequest supplierRequestResponse = supplierService.toDTO(supplier);
+    public ResponseEntity<ApiResponse<List<SupplierResponse>>> getAllSuppliers() {
+        List<Supplier> suppliers = supplierService.findAll();
+        List<SupplierResponse> supplierResponses = suppliers.stream()
+                .map(supplierService::toDTO)
+                .collect(Collectors.toList());
 
-        // TODO: RETURN A SUPPLIER RESPONSE
-
-        return ResponseEntity.ok(ApiResponse.<SupplierRequest>builder()
-                .message("Proveedor guardado.")
-                .data(supplierRequestResponse)
+        return ResponseEntity.ok(ApiResponse.<List<SupplierResponse>>builder()
+                .message("Lista de proveedores.")
+                .data(supplierResponses)
                 .build());
     }
+
+    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<SupplierResponse>> saveSupplier(@Valid @RequestBody SupplierRequest supplierRequest) {
+        Supplier supplier = supplierService.save(supplierRequest);
+        SupplierResponse supplierResponse = supplierService.toDTO(supplier);
+
+        return ResponseEntity.ok(ApiResponse.<SupplierResponse>builder()
+                .message("Proveedor guardado.")
+                .data(supplierResponse)
+                .build());
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<SupplierResponse>> updateSupplier(@PathVariable Integer id, @Valid @RequestBody SupplierRequest supplierRequest) {
+        Supplier supplier = supplierService.update(id, supplierRequest);
+        SupplierResponse supplierResponse = supplierService.toDTO(supplier);
+
+        return ResponseEntity.ok(ApiResponse.<SupplierResponse>builder()
+                .message("Proveedor actualizado.")
+                .data(supplierResponse)
+                .build());
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ApiResponse<SupplierResponse>> deleteSupplier(@PathVariable Integer id) {
+        Supplier supplier = supplierService.delete(id);
+        SupplierResponse supplierResponse = supplierService.toDTO(supplier);
+
+        return ResponseEntity.ok(ApiResponse.<SupplierResponse>builder()
+                .message("Proveedor eliminado.")
+                .data(supplierResponse)
+                .build());
+    }
+
 }
