648755c435a91cecdf49ac28d7f6e4e33d703d20
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 03:48:16 -0600

    Add Lombok annotations and new fields to SupplierResponse
    Introduce Lombok annotations to reduce boilerplate code for getters, setters, constructors, and builders. Added new fields such as `id`, `name`, `phone`, `email`, `address`, `entityTypeName`, `clientContact`, and `idNumber` to enrich the SupplierResponse model.

--- src/main/java/com/impact/core/module/supplier/payload/response/SupplierResponse.java
+++ src/main/java/com/impact/core/module/supplier/payload/response/SupplierResponse.java
@@ -1,4 +1,19 @@
 package com.impact.core.module.supplier.payload.response;
 
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupplierResponse {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String entityTypeName;
+    private String clientContact;
+    private String idNumber;
 }
