61311981c4cf02e831ca4c5054edb1f84c37c889
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 01:22:32 -0600

    Add SupplierRequest class for supplier request payloads
    Introduced a new SupplierRequest class to handle supplier-related request payloads. This includes validation annotations to enforce required fields like name, phone, email, and others. The class also utilizes Lombok annotations for boilerplate code reduction.

---
+++ src/main/java/com/impact/core/module/supplier/payload/request/SupplierRequest.java
@@ -0,0 +1,30 @@
+package com.impact.core.module.supplier.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SupplierRequest {
+    private Integer id;
+    @NotBlank(message = "El nombre es requerido")
+    private String name;
+    @NotBlank(message = "El teléfono es requerido")
+    private String phone;
+    @NotBlank(message = "El email es requerido")
+    @Email(message = "El email no es válido")
+    private String email;
+    @NotBlank(message = "La dirección es requerida")
+    private String address;
+    @NotBlank(message = "El tipo de entidad es requerido")
+    private String entityTypeName;
+    @NotBlank(message = "El contacto del cliente es requerido")
+    private String clientContact;
+    @NotBlank(message = "El número de identificación es requerido")
+    private String idNumber;
+
+}
