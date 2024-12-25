f81228215f2ada193e237a8f5d0d02a276faab84
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 03:47:57 -0600

    Add SupplierMapper to handle entity-DTO conversions
    This commit introduces the SupplierMapper class, responsible for mapping between Supplier entities and their corresponding DTOs. It includes methods to convert SupplierRequest objects to entities, Supplier entities to responses, and retrieve EntityType based on input. This simplifies and centralizes the conversion logic within the supplier module.

---
+++ src/main/java/com/impact/core/module/supplier/mapper/SupplierMapper.java
@@ -0,0 +1,48 @@
+package com.impact.core.module.supplier.mapper;
+
+import com.impact.core.module.supplier.entity.EntityType;
+import com.impact.core.module.supplier.entity.Supplier;
+import com.impact.core.module.supplier.enun.EEntityType;
+import com.impact.core.module.supplier.payload.request.SupplierRequest;
+import com.impact.core.module.supplier.payload.response.SupplierResponse;
+import com.impact.core.module.supplier.service.EntityTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SupplierMapper {
+    public final EntityTypeService entityTypeService;
+
+    public Supplier toEntity(SupplierRequest supplierRequest) {
+        return Supplier.builder()
+                .name(supplierRequest.getName())
+                .phone(supplierRequest.getPhone())
+                .email(supplierRequest.getEmail())
+                .address(supplierRequest.getAddress())
+                .entityType(getEntityType(supplierRequest.getEntityTypeName()))
+                .clientContact(supplierRequest.getClientContact())
+                .idNumber(supplierRequest.getIdNumber())
+                .build();
+    }
+
+    public SupplierResponse toDTO(Supplier supplier) {
+        return SupplierResponse.builder()
+                .id(supplier.getId())
+                .name(supplier.getName())
+                .phone(supplier.getPhone())
+                .email(supplier.getEmail())
+                .address(supplier.getAddress())
+                .entityTypeName(supplier.getEntityType().getTypeName().name())
+                .clientContact(supplier.getClientContact())
+                .idNumber(supplier.getIdNumber())
+                .build();
+    }
+
+    public EntityType getEntityType(String name) {
+        if (name.equalsIgnoreCase("legal")) {
+            return entityTypeService.findByName(EEntityType.TYPE_LEGAL);
+        }
+        return entityTypeService.findByName(EEntityType.TYPE_PHYSICAL);
+    }
+}
