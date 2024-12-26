702735b2649dd3565963a62f4dc6caa872055211
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 19:45:31 -0600

    Add AssetCategoryResponse DTO class
    Introduce AssetCategoryResponse as a Data Transfer Object (DTO) with fields for `id` and `name`. Annotated the class with Lombok for boilerplate code reduction, including setters, getters, builder, no-args, and all-args constructors. This is part of defining the response structure for asset subcategories.

---
+++ src/main/java/com/impact/core/module/assetSubcategory/payload/response/AssetCategoryResponse.java
@@ -0,0 +1,13 @@
+package com.impact.core.module.assetSubcategory.payload.response;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssetCategoryResponse {
+    int id;
+    String name;
+}
