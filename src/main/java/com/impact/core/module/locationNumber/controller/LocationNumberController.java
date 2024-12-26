6c6903af7e5bc46ad59221413fa6cfc490bb0820
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 01:54:00 -0600

    Add CRUD endpoints to LocationNumberController
    Introduce endpoints for creating, retrieving, updating, and deleting location numbers with role-based access control. Include proper request and response handling using DTOs, validation, and standardized API responses.

--- src/main/java/com/impact/core/module/locationNumber/controller/LocationNumberController.java
+++ src/main/java/com/impact/core/module/locationNumber/controller/LocationNumberController.java
@@ -1,13 +1,65 @@
 package com.impact.core.module.locationNumber.controller;
 
+import com.impact.core.module.locationNumber.payload.request.LocationNumberRequest;
+import com.impact.core.module.locationNumber.payload.response.LocationNumberResponse;
 import com.impact.core.module.locationNumber.service.LocationNumberService;
+import com.impact.core.util.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/location-number")
 @RequiredArgsConstructor
 public class LocationNumberController {
     public final LocationNumberService locationNumberService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<List<LocationNumberResponse>>> getAllLocationNumbers() {
+        List<LocationNumberResponse> locationNumberResponses = locationNumberService.findAll();
+
+        return ResponseEntity.ok(ApiResponse.<List<LocationNumberResponse>>builder()
+                .message("Lista de números de ubicación.")
+                .data(locationNumberResponses)
+                .build());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<LocationNumberResponse>> saveLocationNumber(@Valid @RequestBody LocationNumberRequest locationNumberRequest) {
+        LocationNumberResponse locationNumberResponse = locationNumberService.save(locationNumberRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.<LocationNumberResponse>builder()
+                .message("Número de ubicación guardado.")
+                .data(locationNumberResponse)
+                .build());
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<LocationNumberResponse>> updateLocationNumber(@PathVariable int id ,@Valid @RequestBody LocationNumberRequest locationNumberRequest) {
+        LocationNumberResponse locationNumberResponse = locationNumberService.update(id, locationNumberRequest);
+
+        return ResponseEntity.ok(ApiResponse.<LocationNumberResponse>builder()
+                .message("Número de ubicación actualizado.")
+                .data(locationNumberResponse)
+                .build());
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<LocationNumberResponse>> deleteLocationNumber(@PathVariable int id) {
+        LocationNumberResponse locationNumberResponse = locationNumberService.delete(id);
+
+        return ResponseEntity.ok(ApiResponse.<LocationNumberResponse>builder()
+                .message("Número de ubicación eliminado.")
+                .data(locationNumberResponse)
+                .build());
+    }
 }
