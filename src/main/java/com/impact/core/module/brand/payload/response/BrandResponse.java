8a9d8e81462807cef6ef9faa6c910eb12316a1fc
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 21:08:21 -0600

    Add BrandResponse model class
    Introduced a new `BrandResponse` class with Lombok annotations to streamline getters, setters, constructors, and builders. This serves as a response object in the brand module.

---
+++ src/main/java/com/impact/core/module/brand/payload/response/BrandResponse.java
@@ -0,0 +1,13 @@
+package com.impact.core.module.brand.payload.response;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BrandResponse {
+    int id;
+    String name;
+}
