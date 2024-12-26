e4c7697286cbfcd350bb0b96a80038731466e862
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 23:33:44 -0600

    Add ECurrency enum for currency representation
    Introduced ECurrency enum to represent different currency types. Includes a custom toString method for formatting by removing the "CURRENCY" prefix. This enhances readability and prepares for better currency handling.

---
+++ src/main/java/com/impact/core/module/currency/enun/ECurrency.java
@@ -0,0 +1,11 @@
+package com.impact.core.module.currency.enun;
+
+public enum ECurrency {
+    CURRENCY_COLON,
+    CURRENCY_DOLLAR;
+
+    @Override
+    public String toString() {
+        return this.name().replaceFirst("CURRENCY", "");
+    }
+}
