e8df7326395946739116eb06198fa8592ceae60e
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-25 23:46:45 -0600

    Add CurrencyResponse DTO class
    Introduce a new CurrencyResponse DTO to encapsulate currency data, including id, code, symbol, and stateName. This addition streamlines data handling and improves maintainability in the currency module.

--- src/main/java/com/impact/core/module/currency/service/CurrencyService.java
+++ src/main/java/com/impact/core/module/currency/service/CurrencyService.java
@@ -3,17 +3,28 @@ package com.impact.core.module.currency.service;
 import com.impact.core.expection.customException.ResourceNotFoundException;
 import com.impact.core.module.currency.entity.Currency;
 import com.impact.core.module.currency.enun.ECurrency;
+import com.impact.core.module.currency.mapper.CurrencyMapper;
+import com.impact.core.module.currency.payload.response.CurrencyResponse;
 import com.impact.core.module.currency.repository.CurrencyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("currencyService")
 @RequiredArgsConstructor
 public class CurrencyService {
     public final CurrencyRepository currencyRepository;
+    public final CurrencyMapper currencyMapper;
 
     public Currency findByCurrencyName(ECurrency currencyName) {
         return currencyRepository.findByCurrencyName(currencyName)
                 .orElseThrow(() -> new ResourceNotFoundException("El tipo de moneda : " + currencyName + " no se encuentra en la base de datos"));
+    }
+
+    public List<CurrencyResponse> findAll() {
+        return currencyRepository.findAll().stream()
+                .map(currencyMapper::toDTO)
+                .toList();
     }
 }
