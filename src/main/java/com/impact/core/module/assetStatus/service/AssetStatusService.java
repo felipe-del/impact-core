6b1efa1727f1ef6a06a524a5a9affbe590483e61
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 00:03:42 -0600

    Add methods to fetch asset status by name and list all
    Introduce `findByName` to retrieve an asset status by its name, throwing a `ResourceNotFoundException` if not found. Add `findAll` method to return all asset statuses as DTOs using the mapper. These changes enhance the service's data retrieval capabilities.

--- src/main/java/com/impact/core/module/assetStatus/service/AssetStatusService.java
+++ src/main/java/com/impact/core/module/assetStatus/service/AssetStatusService.java
@@ -1,13 +1,32 @@
 package com.impact.core.module.assetStatus.service;
 
+import com.impact.core.expection.customException.ResourceNotFoundException;
+import com.impact.core.module.assetStatus.entity.AssetStatus;
+import com.impact.core.module.assetStatus.enun.EAssetStatus;
+import com.impact.core.module.assetStatus.mapper.AssetStatusMapper;
+import com.impact.core.module.assetStatus.payload.response.AssetStatusResponse;
 import com.impact.core.module.assetStatus.repository.AssetStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service("assetStatusService")
 @RequiredArgsConstructor
 public class AssetStatusService {
     public final AssetStatusRepository assetStatusRepository;
+    public final AssetStatusMapper assetStatusMapper;
 
+    public AssetStatus findByName(EAssetStatus name) {
+        return assetStatusRepository.findByName(name)
+                .orElseThrow(() -> new ResourceNotFoundException("El estado: " + name + " no existe en la base de datos."));
+    }
+
+    public List<AssetStatusResponse> findAll() {
+        return assetStatusRepository.findAll().stream()
+                .map(assetStatusMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 
 }
