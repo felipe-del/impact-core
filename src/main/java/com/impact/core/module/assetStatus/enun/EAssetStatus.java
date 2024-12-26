02aaf2c60933f486b9a5554ecb586736e4833fdb
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 00:03:54 -0600

    Add EAssetStatus enum for asset status management
    Introduce the EAssetStatus enum to define and standardize asset status values like AVAILABLE, IN_MAINTENANCE, LOANED, and OUT_OF_SERVICE. Includes a custom `toString()` method to format enum names for better readability.

---
+++ src/main/java/com/impact/core/module/assetStatus/enun/EAssetStatus.java
@@ -0,0 +1,13 @@
+package com.impact.core.module.assetStatus.enun;
+
+public enum EAssetStatus {
+    ASSET_STATUS_AVAILABLE,
+    ASSET_STATUS_IN_MAINTENANCE,
+    ASSET_STATUS_LOANED,
+    ASSET_STATUS_OUT_OF_SERVICE;
+
+    @Override
+    public String toString() {
+        return name().replace("ASSET_STATUS_", " ");
+    }
+}
