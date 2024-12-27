29e725f2707e7435b08788b538d2a6b20afb8a27
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 18:48:33 -0600

    Add BuildingResponse class to handle building data responses
    This commit introduces the BuildingResponse class in the specified package. It is annotated with Lombok to simplify boilerplate code and provides a structure for response payloads in the building location module.

---
+++ src/main/java/com/impact/core/module/buildingLocation/payload/response/BuildingResponse.java
@@ -0,0 +1,11 @@
+package com.impact.core.module.buildingLocation.payload.response;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BuildingResponse {
+}
