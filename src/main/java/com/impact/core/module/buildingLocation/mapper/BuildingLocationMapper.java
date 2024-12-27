9a42e38e0fec2e42a3f921e0016dc470fd3afc8a
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:43:39 -0600

    Add mapping methods to BuildingLocationMapper
    Introduced methods to convert between BuildingLocation, BuildingLocationRequest, and BuildingLocationResponse. These changes facilitate data transformation and integration with BuildingService. Added necessary dependencies and annotations to support the new functionality.

--- src/main/java/com/impact/core/module/buildingLocation/mapper/BuildingLocationMapper.java
+++ src/main/java/com/impact/core/module/buildingLocation/mapper/BuildingLocationMapper.java
@@ -1,7 +1,31 @@
 package com.impact.core.module.buildingLocation.mapper;
 
+import com.impact.core.module.buildingLocation.entity.BuildingLocation;
+import com.impact.core.module.buildingLocation.payload.request.BuildingLocationRequest;
+import com.impact.core.module.buildingLocation.payload.response.BuildingLocationResponse;
+import com.impact.core.module.buildingLocation.service.BuildingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BuildingLocationMapper {
+
+    public final BuildingService buildingService;
+    public final BuildingMapper buildingMapper;
+
+    public BuildingLocation toEntity(BuildingLocationRequest buildingLocationRequest) {
+        return BuildingLocation.builder()
+                .building(buildingService.findById(buildingLocationRequest.getBuildingId()))
+                .floor(buildingLocationRequest.getFloor())
+                .build();
+    }
+
+    public BuildingLocationResponse toDTO(BuildingLocation buildingLocation) {
+        return BuildingLocationResponse.builder()
+                .id(buildingLocation.getId())
+                .building(buildingMapper.toDTO(buildingLocation.getBuilding()))
+                .floor(buildingLocation.getFloor())
+                .build();
+    }
 }
