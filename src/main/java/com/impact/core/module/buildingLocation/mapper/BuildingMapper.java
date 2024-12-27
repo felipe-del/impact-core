93a1b6104cf7ac605e8c8f64e4f95ab0f2f2d21b
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:44:07 -0600

    Add mapping methods to BuildingMapper
    Introduced `toEntity` and `toDTO` methods in `BuildingMapper` to simplify conversions between `BuildingRequest`, `BuildingResponse`, and `Building` entities. This enhances code modularity and reduces redundancy in transformation logic.

--- src/main/java/com/impact/core/module/buildingLocation/mapper/BuildingMapper.java
+++ src/main/java/com/impact/core/module/buildingLocation/mapper/BuildingMapper.java
@@ -1,7 +1,23 @@
 package com.impact.core.module.buildingLocation.mapper;
 
+import com.impact.core.module.buildingLocation.entity.Building;
+import com.impact.core.module.buildingLocation.payload.request.BuildingRequest;
+import com.impact.core.module.buildingLocation.payload.response.BuildingResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BuildingMapper {
+
+    public Building toEntity(BuildingRequest buildingRequest) {
+        return Building.builder()
+                .name(buildingRequest.getName())
+                .build();
+    }
+
+    public BuildingResponse toDTO(Building building) {
+        return BuildingResponse.builder()
+                .id(building.getId())
+                .name(building.getName())
+                .build();
+    }
 }
