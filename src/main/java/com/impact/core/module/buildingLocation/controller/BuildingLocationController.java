a91170aeb466ed0812c7644e56e868fe0168c680
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 18:47:41 -0600

    Add BuildingLocationController class
    Introduce a new controller for handling requests related to building locations. This sets up the basic structure with REST annotations and a base API endpoint.

---
+++ src/main/java/com/impact/core/module/buildingLocation/controller/BuildingLocationController.java
@@ -0,0 +1,11 @@
+package com.impact.core.module.buildingLocation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/building-location")
+@RequiredArgsConstructor
+public class BuildingLocationController {
+}
