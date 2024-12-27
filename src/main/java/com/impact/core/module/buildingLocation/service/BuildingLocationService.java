5d5c79094d3957975cdce8a290e1e22e9cfb3a3b
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 18:48:09 -0600

    Add BuildingLocationService class
    Introduce a new service class for building location functionality. This establishes the necessary structure for future feature development and integration. Annotations like @Service and @RequiredArgsConstructor are applied.

---
+++ src/main/java/com/impact/core/module/buildingLocation/service/BuildingLocationService.java
@@ -0,0 +1,9 @@
+package com.impact.core.module.buildingLocation.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service("buildingLocationService")
+@RequiredArgsConstructor
+public class BuildingLocationService {
+}
