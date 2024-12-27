d2aac949b22bb359fc82c455a5940e6ebeb515ae
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:43:49 -0600

    Add validation annotations to BuildingLocationRequest
    Added `@NotNull`, `@NotBlank`, and `@Size` annotations to enforce constraints on `buildingId` and `floor` fields. This ensures the input data is validated correctly, preventing null values and exceeding length limits.

--- src/main/java/com/impact/core/module/buildingLocation/payload/request/BuildingLocationRequest.java
+++ src/main/java/com/impact/core/module/buildingLocation/payload/request/BuildingLocationRequest.java
@@ -1,5 +1,8 @@
 package com.impact.core.module.buildingLocation.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Setter
@@ -8,4 +11,9 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BuildingLocationRequest {
+    @NotNull(message = "El id del edificio no puede estar vacío")
+    int buildingId;
+    @NotBlank(message = "El piso no puede estar vacío")
+    @Size(max = 50, message = "El piso no puede tener más de 50 caracteres")
+    String floor;
 }
