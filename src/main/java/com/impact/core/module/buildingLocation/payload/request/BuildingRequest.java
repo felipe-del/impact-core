33cc17596499336b052bfc7469ef42ea7a786dfb
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:44:16 -0600

    Add validation annotations to BuildingRequest name field
    Introduced @NotBlank and @Size annotations to validate the "name" field in BuildingRequest. These constraints ensure the field is not empty and its length does not exceed 100 characters. This enhances data integrity and prevents invalid inputs.

--- src/main/java/com/impact/core/module/buildingLocation/payload/request/BuildingRequest.java
+++ src/main/java/com/impact/core/module/buildingLocation/payload/request/BuildingRequest.java
@@ -1,5 +1,7 @@
 package com.impact.core.module.buildingLocation.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Setter
@@ -8,4 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BuildingRequest {
+    @NotBlank(message = "El nombre del edificio no puede estar vacío")
+    @Size(max = 100, message = "El nombre del edificio no puede tener más de 100 caracteres")
+    String name;
 }
