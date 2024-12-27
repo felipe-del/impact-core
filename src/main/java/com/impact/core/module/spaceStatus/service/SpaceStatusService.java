3dc6994f81b02dfc47c8302a8210158b9d3c8e54
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-27 00:37:53 -0600

    Add SpaceStatusService to manage space status operations
    This service provides methods to retrieve all space statuses and find a specific space status by name. It integrates with the repository and mapper layers, handling potential not-found exceptions gracefully.

---
+++ src/main/java/com/impact/core/module/spaceStatus/service/SpaceStatusService.java
@@ -0,0 +1,31 @@
+package com.impact.core.module.spaceStatus.service;
+
+import com.impact.core.expection.customException.ResourceNotFoundException;
+import com.impact.core.module.spaceStatus.entity.SpaceStatus;
+import com.impact.core.module.spaceStatus.enun.ESpaceStatus;
+import com.impact.core.module.spaceStatus.mapper.SpaceStatusMapper;
+import com.impact.core.module.spaceStatus.payload.response.SpaceStatusResponse;
+import com.impact.core.module.spaceStatus.repository.SpaceStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("spaceStatusService")
+@RequiredArgsConstructor
+public class SpaceStatusService {
+    public final SpaceStatusRepository spaceStatusRepository;
+    public final SpaceStatusMapper spaceStatusMapper;
+
+    public List<SpaceStatusResponse> findAll() {
+        return spaceStatusRepository.findAll().stream()
+                .map(spaceStatusMapper::toDTO)
+                .toList();
+    }
+
+    public SpaceStatus findByName(ESpaceStatus name) {
+        return spaceStatusRepository.findByName(name)
+                .orElseThrow(() -> new ResourceNotFoundException("El estado de espacio : " + name + " no se encuentra en la base de datos"));
+    }
+
+}
