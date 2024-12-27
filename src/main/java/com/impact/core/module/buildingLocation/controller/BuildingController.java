77db8b35ae67fc472d633e3e0ff5a0917aecd214
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:43:27 -0600

    Add CRUD endpoints to BuildingController
    Introduced endpoints for creating, reading, updating, and deleting building records with appropriate request and response handling. Each method is secured with role-based access control for administrators and managers. This integration enhances API functionality for managing buildings effectively.

--- src/main/java/com/impact/core/module/buildingLocation/controller/BuildingController.java
+++ src/main/java/com/impact/core/module/buildingLocation/controller/BuildingController.java
@@ -1,11 +1,65 @@
 package com.impact.core.module.buildingLocation.controller;
 
+import com.impact.core.module.buildingLocation.payload.request.BuildingRequest;
+import com.impact.core.module.buildingLocation.payload.response.BuildingResponse;
+import com.impact.core.module.buildingLocation.service.BuildingService;
+import com.impact.core.util.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/building")
 @RequiredArgsConstructor
 public class BuildingController {
+    public final BuildingService buildingService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<List<BuildingResponse>>> getAllBuildings() {
+        List<BuildingResponse> buildingResponses = buildingService.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.<List<BuildingResponse>>builder()
+                .message("Lista de edificios")
+                .data(buildingResponses)
+                .build());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<BuildingResponse>> saveBuilding(@Valid @RequestBody BuildingRequest buildingRequest) {
+        BuildingResponse buildingResponse = buildingService.save(buildingRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.<BuildingResponse>builder()
+                .message("Edificio creado exitosamente")
+                .data(buildingResponse)
+                .build());
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<BuildingResponse>> updateBuilding(@PathVariable Integer id, @Valid @RequestBody BuildingRequest buildingRequest) {
+        BuildingResponse buildingResponse = buildingService.update(id, buildingRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.<BuildingResponse>builder()
+                .message("Edificio actualizado exitosamente")
+                .data(buildingResponse)
+                .build());
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<BuildingResponse>> deleteBuilding(@PathVariable Integer id) {
+        BuildingResponse buildingResponse = buildingService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.<BuildingResponse>builder()
+                .message("Edificio eliminado exitosamente")
+                .data(buildingResponse)
+                .build());
+    }
 }
