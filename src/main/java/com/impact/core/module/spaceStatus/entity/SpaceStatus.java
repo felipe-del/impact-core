259904b60a8d351d982c62cafa1d4db8a2a72408
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-27 00:37:24 -0600

    Rename and refactor SpaceStatus entity for better structure
    Moved SpaceStatus to a more structured package and refactored `name` field to use the ESpaceStatus enum. Also added `@GeneratedValue` annotation for the ID field to support auto-generation. These changes improve clarity and alignment with the module structure.

--- src/main/java/com/impact/core/module/spaceStatus/entity/SpaceStatus.java
+++ src/main/java/com/impact/core/module/spaceStatus/entity/SpaceStatus.java
@@ -1,5 +1,6 @@
-package com.impact.core.entities;
+package com.impact.core.module.spaceStatus.entity;
 
+import com.impact.core.module.spaceStatus.enun.ESpaceStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,12 +14,14 @@ import lombok.Setter;
 public class SpaceStatus {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Size(max = 50)
     @NotNull
     @Column(name = "name", nullable = false, length = 50)
-    private String name;
+    @Enumerated(EnumType.STRING)
+    private ESpaceStatus name;
 
     @Lob
     @Column(name = "description")
