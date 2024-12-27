e655a0e8dfbe3085619bf39b180f37dc68103136
felipe-del <isaacfelibrenes1904@gmail.com>
2024-12-26 21:43:58 -0600

    Add fields to BuildingLocationResponse for enhanced payload
    Added `id`, `building`, and `floor` fields to the `BuildingLocationResponse` class. These changes support more detailed data representation and improve compatibility with `BuildingRequest`.

--- src/main/java/com/impact/core/module/buildingLocation/payload/response/BuildingLocationResponse.java
+++ src/main/java/com/impact/core/module/buildingLocation/payload/response/BuildingLocationResponse.java
@@ -1,5 +1,6 @@
 package com.impact.core.module.buildingLocation.payload.response;
 
+import com.impact.core.module.buildingLocation.payload.request.BuildingRequest;
 import lombok.*;
 
 @Setter
@@ -8,4 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BuildingLocationResponse {
+    int id;
+    BuildingResponse building;
+    String floor;
 }
