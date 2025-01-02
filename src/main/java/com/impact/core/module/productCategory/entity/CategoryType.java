eb735e8ed2af977b1f74ef1aa4d661c9a75a858d
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-02 01:53:07 -0600

    Rename and refactor CategoryType entity location and annotations
    Moved CategoryType to a more appropriate package for product category entities, adhering to modular structure. Updated annotations to include builder pattern support and automatic ID generation. These changes improve code organization, maintainability, and scalability.

--- src/main/java/com/impact/core/module/productCategory/entity/CategoryType.java
+++ src/main/java/com/impact/core/module/productCategory/entity/CategoryType.java
@@ -1,21 +1,21 @@
-package com.impact.core.entities;
+package com.impact.core.module.productCategory.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "category_type")
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryType {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Size(max = 50)
