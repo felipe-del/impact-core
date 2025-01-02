93a09524f3fe4764eb92244ecc147888212bf2cb
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-02 16:32:20 -0600

    Refactor ProductStatus structure and enhance functionality.
    Renamed and moved ProductStatus to a new module-specific package for better organization. Added Lombok annotations for cleaner code and replaced the `name` field type with an enumerated type `EProductStatus` to improve data consistency.

--- src/main/java/com/impact/core/module/productStatus/entity/ProductStatus.java
+++ src/main/java/com/impact/core/module/productStatus/entity/ProductStatus.java
@@ -1,15 +1,18 @@
-package com.impact.core.entities;
+package com.impact.core.module.productStatus.entity;
 
+import com.impact.core.module.productStatus.enun.EProductStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "product_status")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductStatus {
     @Id
     @Column(name = "id", nullable = false)
@@ -18,7 +21,8 @@ public class ProductStatus {
     @Size(max = 50)
     @NotNull
     @Column(name = "name", nullable = false, length = 50)
-    private String name;
+    @Enumerated(EnumType.STRING)
+    private EProductStatus name;
 
     @Lob
     @Column(name = "description")
