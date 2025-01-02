e4b26174c9493367315dff1058bcaeb167afaad3
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-02 01:55:58 -0600

    Add UnitOfMeasurementController to handle unit operations
    Introduced a new REST controller to manage retrieval of unit of measurement data. The endpoint is restricted to users with ADMINISTRATOR or MANAGER roles and returns a list of units in a standardized API response format.

---
+++ src/main/java/com/impact/core/module/productCategory/controller/UnitOfMeasurementController.java
@@ -0,0 +1,31 @@
+package com.impact.core.module.productCategory.controller;
+
+import com.impact.core.module.productCategory.payload.response.UnitOfMeasurementResponse;
+import com.impact.core.module.productCategory.service.UnitOfMeasurementService;
+import com.impact.core.util.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/unit-of-measurement")
+@RequiredArgsConstructor
+public class UnitOfMeasurementController {
+    public final UnitOfMeasurementService unitOfMeasurementService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<List<UnitOfMeasurementResponse>>> getAllUnitOfMeasurements() {
+        List<UnitOfMeasurementResponse> unitOfMeasurementResponses = unitOfMeasurementService.findAll();
+
+        return ResponseEntity.ok(ApiResponse.<List<UnitOfMeasurementResponse>>builder()
+                .message("Lista de unidades de medida.")
+                .data(unitOfMeasurementResponses)
+                .build());
+    }
+}
