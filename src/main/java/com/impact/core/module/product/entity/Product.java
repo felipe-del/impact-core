8f79faa8e042ae7eed9152d542f56e691dce2295
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-02 23:06:53 -0600

    Rename and refactor Product entity for modular structure
    Renamed Product.java to align with the new modular directory structure. Added annotations (@Builder, @NoArgsConstructor, @AllArgsConstructor) to enhance entity creation flexibility. Introduced a new "name" field to the Product entity for better data representation.

--- src/main/java/com/impact/core/module/product/entity/Product.java
+++ src/main/java/com/impact/core/module/product/entity/Product.java
@@ -1,21 +1,27 @@
-package com.impact.core.entities;
+package com.impact.core.module.product.entity;
 
 import com.impact.core.module.productCategory.entity.ProductCategory;
 import com.impact.core.module.productStatus.entity.ProductStatus;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @Column(name = "purchase_date")
     private LocalDate purchaseDate;
