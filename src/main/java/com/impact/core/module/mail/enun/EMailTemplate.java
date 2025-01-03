c10fd323be427cb1ae1b401775b94d6a98f1465b
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 14:08:33 -0600

    Update email templates and add a generic template
    Renamed the forgot password template to reset password for clarity. Added a new generic email template to support more versatile use cases.

--- src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
+++ src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum EMailTemplate {
     WELCOME_EMAIL("new_user_welcome_email_template"),
-    FORGOT_PASSWORD_EMAIL("forgot_password_email_template");
+    RESET_PASSWORD_EMAIL("reset_password_email_template"),
+    GENERIC_EMAIL("generic_email_template");
 
     private final String templateName;
 
