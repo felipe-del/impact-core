7e820982a56916d96afe4f772de0463e6634aa85
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 12:47:11 -0600

    Add ProductRequestDTORequest class for product request payload
    This new class includes fields for productId, statusName, and reason with validation annotations to enforce required constraints. It utilizes Lombok for boilerplate code reduction and adheres to clean coding practices.

---
+++ src/main/java/com/impact/core/module/productRequest/payload/request/ProductRequestDTORequest.java
@@ -0,0 +1,19 @@
+package com.impact.core.module.productRequest.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequestDTORequest {
+    @NotNull(message = "El id del producto es requerido")
+    private int productId;
+    @NotBlank(message = "El nombre del estado es requerido")
+    private String statusName;
+    @NotBlank(message = "La razón es requerida")
+    private String reason;
+}
