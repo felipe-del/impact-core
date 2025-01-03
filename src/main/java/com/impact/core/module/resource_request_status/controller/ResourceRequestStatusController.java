8e6e44d9cde4247b7a3097c4755d7ec1f96e9117
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 11:37:30 -0600

    Add ResourceRequestStatusController to handle status retrieval
    This controller provides an endpoint to fetch all resource request statuses. Access is restricted to users with ADMINISTRATOR or MANAGER roles, ensuring proper security and role-based access control.

---
+++ src/main/java/com/impact/core/module/resource_request_status/controller/ResourceRequestStatusController.java
@@ -0,0 +1,31 @@
+package com.impact.core.module.resource_request_status.controller;
+
+import com.impact.core.module.resource_request_status.payload.response.ResourceRequestStatusResponse;
+import com.impact.core.module.resource_request_status.service.ResourceRequestStatusService;
+import com.impact.core.util.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/resource-request-status")
+@RequiredArgsConstructor
+public class ResourceRequestStatusController {
+    public final ResourceRequestStatusService resourceRequestStatusService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<List<ResourceRequestStatusResponse>>> getAllResourceRequestStatus() {
+        List<ResourceRequestStatusResponse> resourceRequestStatusResponses = resourceRequestStatusService.findAll();
+
+        return ResponseEntity.ok(ApiResponse.<List<ResourceRequestStatusResponse>>builder()
+                .message("Lista de estados de solicitud de recursos.")
+                .data(resourceRequestStatusResponses)
+                .build());
+    }
+}
