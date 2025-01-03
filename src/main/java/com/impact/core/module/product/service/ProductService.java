7c5b26d0a510a106ab7ed4237d3593c9fe8ed308
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 12:47:53 -0600

    Refactor ProductService to use DTO for requests and responses.
    Replaced ProductRequest and ProductResponse with ProductRequestDTO and ProductResponseDTO for consistency and clarity. Updated method signatures and associated mapping logic to align with the new DTO-based structure. This change improves maintainability and enforces a standardized model across the service.

--- src/main/java/com/impact/core/module/product/service/ProductService.java
+++ src/main/java/com/impact/core/module/product/service/ProductService.java
@@ -3,8 +3,8 @@ package com.impact.core.module.product.service;
 import com.impact.core.expection.customException.ResourceNotFoundException;
 import com.impact.core.module.product.entity.Product;
 import com.impact.core.module.product.mapper.ProductMapper;
-import com.impact.core.module.product.payload.request.ProductRequest;
-import com.impact.core.module.product.payload.response.ProductResponse;
+import com.impact.core.module.product.payload.request.ProductRequestDTO;
+import com.impact.core.module.product.payload.response.ProductResponseDTO;
 import com.impact.core.module.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,21 +18,21 @@ public class ProductService {
     public final ProductRepository productRepository;
     public final ProductMapper productMapper;
 
-    public ProductResponse save(ProductRequest productRequest) {
-        Product product = productMapper.toEntity(productRequest);
+    public ProductResponseDTO save(ProductRequestDTO productRequestDTO) {
+        Product product = productMapper.toEntity(productRequestDTO);
         Product productSaved = productRepository.save(product);
         return productMapper.toDTO(productSaved);
     }
 
-    public ProductResponse update(int id, ProductRequest productRequest) {
+    public ProductResponseDTO update(int id, ProductRequestDTO productRequestDTO) {
         Product product = findProductById(id);
-        Product productUpdated = productMapper.toEntity(productRequest);
+        Product productUpdated = productMapper.toEntity(productRequestDTO);
         productUpdated.setId(product.getId());
         Product productSaved = productRepository.save(productUpdated);
         return productMapper.toDTO(productSaved);
     }
 
-    public ProductResponse delete(int id) {
+    public ProductResponseDTO delete(int id) {
         Product product = findProductById(id);
         productRepository.delete(product);
         return productMapper.toDTO(product);
@@ -43,7 +43,7 @@ public class ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("El producto con el id: " + id + " no existe en la base de datos."));
     }
 
-    public List<ProductResponse> findAll() {
+    public List<ProductResponseDTO> findAll() {
         return productRepository.findAll().stream()
                 .map(productMapper::toDTO)
                 .collect(Collectors.toList());
