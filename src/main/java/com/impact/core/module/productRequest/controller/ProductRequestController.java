c2b133e332a3731edc734f701833ff9024e4ce07
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 12:46:57 -0600

    Rename and update ProductRequest entity structure.
    Moved ProductRequest to a new package for better organization and added annotations for constructors and builder pattern. Introduced a new association with the User entity to enhance functionality and added appropriate mapping annotations.

---
+++ src/main/java/com/impact/core/module/productRequest/controller/ProductRequestController.java
@@ -0,0 +1,69 @@
+package com.impact.core.module.productRequest.controller;
+
+import com.impact.core.module.productRequest.payload.request.ProductRequestDTORequest;
+import com.impact.core.module.productRequest.payload.response.ProductRequestDTOResponse;
+import com.impact.core.module.productRequest.service.ProductRequestService;
+import com.impact.core.security.service.UserDetailsImpl;
+import com.impact.core.util.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product-request")
+@RequiredArgsConstructor
+public class ProductRequestController {
+    public final ProductRequestService productRequestService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<List<ProductRequestDTOResponse>>> getAllProductRequests() {
+        List<ProductRequestDTOResponse> productRequestDTOResponses = productRequestService.findAll();
+
+        return ResponseEntity.ok(ApiResponse.<List<ProductRequestDTOResponse>>builder()
+                .message("Lista de solicitudes de productos.")
+                .data(productRequestDTOResponses)
+                .build());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<ProductRequestDTOResponse>> saveProductRequest(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @Valid @RequestBody ProductRequestDTORequest productRequestDTORequest) {
+        ProductRequestDTOResponse ProductRequestDTOResponse = productRequestService.save(userDetails, productRequestDTORequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.<ProductRequestDTOResponse>builder()
+                .message("Solicitud de producto guardada correctamente.")
+                .data(ProductRequestDTOResponse)
+                .build());
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<ProductRequestDTOResponse>> updateProductRequest(@PathVariable int id, @Valid @RequestBody ProductRequestDTORequest productRequestDTORequest) {
+        ProductRequestDTOResponse ProductRequestDTOResponse = productRequestService.update(id, productRequestDTORequest);
+
+        return ResponseEntity.ok(ApiResponse.<ProductRequestDTOResponse>builder()
+                .message("Solicitud de producto actualizada correctamente.")
+                .data(ProductRequestDTOResponse)
+                .build());
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRATOR') or hasRole('MANAGER')")
+    public ResponseEntity<ApiResponse<ProductRequestDTOResponse>> deleteProductRequest(@PathVariable int id) {
+        ProductRequestDTOResponse ProductRequestDTOResponse = productRequestService.delete(id);
+
+        return ResponseEntity.ok(ApiResponse.<ProductRequestDTOResponse>builder()
+                .message("Solicitud de producto eliminada correctamente.")
+                .data(ProductRequestDTOResponse)
+                .build());
+    }
+}
