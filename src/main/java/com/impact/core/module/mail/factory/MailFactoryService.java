86c07fc7094db42d043c41df3b12a17ebed1e939
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-03 14:09:03 -0600

    Add email creation for product request submissions
    Introduced a new method to generate and send emails for product request submissions, including relevant product and request details. Updated metadata handling and adjusted templates to provide improved email customization.

--- src/main/java/com/impact/core/module/mail/factory/MailFactoryService.java
+++ src/main/java/com/impact/core/module/mail/factory/MailFactoryService.java
@@ -3,6 +3,8 @@ package com.impact.core.module.mail.factory;
 import com.impact.core.module.mail.payload.ComposedMail;
 import com.impact.core.module.mail.payload.MetaData;
 import com.impact.core.module.mail.enun.EMailTemplate;
+import com.impact.core.module.product.entity.Product;
+import com.impact.core.module.productRequest.entity.ProductRequest;
 import com.impact.core.module.user.entity.User;
 import com.impact.core.module.user.entity.UserToken;
 
@@ -38,11 +40,40 @@ public class MailFactoryService {
         return ComposedMail.builder()
                 .to(user.getEmail())
                 .subject("Restablecer contraseña")
-                .template(EMailTemplate.FORGOT_PASSWORD_EMAIL.getTemplateName())
+                .template(EMailTemplate.RESET_PASSWORD_EMAIL.getTemplateName())
                 .metaData(metaData)
                 .imageNames(List.of(
                         "UCR_CIMPA_LOGO.png"
                 ))
                 .build();
     }
+
+    public static ComposedMail createRequestSentEmail(ProductRequest productRequest){
+        User user = productRequest.getUser();
+        Product product = productRequest.getProduct();
+
+        String emailContent = "Se ha enviado una solicitud de producto con los siguientes detalles: \n" +
+                "Producto: " + product.getName() + "\n" +
+                "Razón: " + productRequest.getReason() + "\n" +
+                "Estado: " + productRequest.getStatus().getName() + "\n" +
+                "Fecha de creación: " + productRequest.getCreatedAt().toString() + "\n" +
+                "Por favor, espere a que se procese su solicitud.";
+
+        List<MetaData> metaData = new ArrayList<>();
+
+        metaData.add(new MetaData("emailTitle", "Solicitud de producto enviada"));
+        metaData.add(new MetaData("userName", user.getName()));
+        metaData.add(new MetaData("emailContent", emailContent));
+
+        return ComposedMail.builder()
+                .to(user.getEmail())
+                .subject("Solicitud de producto enviada")
+                .template(EMailTemplate.GENERIC_EMAIL.getTemplateName())
+                .metaData(metaData)
+                .imageNames(List.of(
+                        "UCR_CIMPA_LOGO.png"
+                ))
+                .build();
+    }
+
 }
