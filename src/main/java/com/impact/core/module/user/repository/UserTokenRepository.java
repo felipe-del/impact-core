2061fcda8b94bb7b4b4a1d310d108495f9ef86d9
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-04 23:29:41 -0600

    Add method to find UserToken by User
    Added a new method `findByUser` in `UserTokenRepository` to enable retrieval of user tokens based on the associated user. This enhances the repository's querying capability, improving flexibility for user-token-related operations.

--- src/main/java/com/impact/core/module/user/repository/UserTokenRepository.java
+++ src/main/java/com/impact/core/module/user/repository/UserTokenRepository.java
@@ -1,5 +1,6 @@
 package com.impact.core.module.user.repository;
 
+import com.impact.core.module.user.entity.User;
 import com.impact.core.module.user.entity.UserToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserTokenRepository extends JpaRepository<UserToken, Long> {
     Optional<UserToken> findByToken(String token);
+    Optional<UserToken> findByUser(User user);
     boolean existsByToken(String token);
 }
