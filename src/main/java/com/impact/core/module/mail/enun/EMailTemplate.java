d19adb7148036a20301c723b66aab11554df90b3
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-04 20:52:11 -0600

    Simplify email template enumeration.
    Removed redundant template entries for welcome and reset password emails, leaving only the generic template. This streamlines the enum for better maintainability and reduces unnecessary complexity.

--- src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
+++ src/main/java/com/impact/core/module/mail/enun/EMailTemplate.java
@@ -4,14 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum EMailTemplate {
-    WELCOME_EMAIL("new_user_welcome_email_template"),
-    RESET_PASSWORD_EMAIL("reset_password_email_template"),
-    GENERIC_EMAIL("generic_email_template");
+    GENERIC_EMAIL("generic_email");
 
     private final String templateName;
 
     EMailTemplate(String templateName) {
         this.templateName = templateName;
     }
-
 }
