d4fd5ac31585e3b45edff93b80ac3ff9c4d92829
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-05 02:09:10 -0600

    Simplify status mapping in ProductRequestMapper.
    Replaced the private method for status mapping with a direct call to `resourceRequestStatusService.findByName()`, defaulting to the `EARRING` status. This reduces complexity and improves maintainability by removing unnecessary code.

--- src/main/java/com/impact/core/module/productRequest/mapper/ProductRequestMapper.java
+++ src/main/java/com/impact/core/module/productRequest/mapper/ProductRequestMapper.java
@@ -6,13 +6,11 @@ import com.impact.core.module.product.service.ProductService;
 import com.impact.core.module.productRequest.entity.ProductRequest;
 import com.impact.core.module.productRequest.payload.request.ProductRequestDTORequest;
 import com.impact.core.module.productRequest.payload.response.ProductRequestDTOResponse;
-import com.impact.core.module.resource_request_status.entity.ResourceRequestStatus;
 import com.impact.core.module.resource_request_status.enun.EResourceRequestStatus;
 import com.impact.core.module.resource_request_status.mapper.ResourceRequestStatusMapper;
 import com.impact.core.module.resource_request_status.payload.response.ResourceRequestStatusResponse;
 import com.impact.core.module.resource_request_status.service.ResourceRequestStatusService;
 import com.impact.core.module.user.mapper.MyUserMapper;
-import com.impact.core.module.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -29,7 +27,7 @@ public class ProductRequestMapper {
     public ProductRequest toEntity(ProductRequestDTORequest productRequestDTORequest) {
         return ProductRequest.builder()
                 .product(productService.findProductById(productRequestDTORequest.getProductId()))
-                .status(this.getResourceRequestStatus(productRequestDTORequest.getStatusName()))
+                .status(resourceRequestStatusService.findByName(EResourceRequestStatus.RESOURCE_REQUEST_STATUS_EARRING))
                 .reason(productRequestDTORequest.getReason())
                 .build();
     }
@@ -48,15 +46,4 @@ public class ProductRequestMapper {
                 .build();
     }
 
-
-    // PRIVATE METHODS
-
-    private ResourceRequestStatus getResourceRequestStatus(String statusName) {
-        return switch (statusName.toLowerCase()) {
-            case "canceled" -> resourceRequestStatusService.findByName(EResourceRequestStatus.RESOURCE_REQUEST_STATUS_CANCELED);
-            case "accepted" -> resourceRequestStatusService.findByName(EResourceRequestStatus.RESOURCE_REQUEST_STATUS_ACCEPTED);
-            case "returned" -> resourceRequestStatusService.findByName(EResourceRequestStatus.RESOURCE_REQUEST_STATUS_RETURNED);
-            default -> resourceRequestStatusService.findByName(EResourceRequestStatus.RESOURCE_REQUEST_STATUS_EARRING);
-        };
-    }
 }
