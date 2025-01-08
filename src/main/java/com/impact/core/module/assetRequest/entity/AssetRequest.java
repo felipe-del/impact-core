32af28f5c4319059eb6807c9c9a5ced040d09e32
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-08 01:24:37 -0600

    Rename and enhance AssetRequest entity with new fields
    Renamed the AssetRequest file path and package for clearer module structuring. Updated the corresponding table name and added `user` and `createdAt` fields with necessary annotations for better functionality. Included validation and default value settings.
    
    Took 27 minutes

--- src/main/java/com/impact/core/module/assetRequest/entity/AssetRequest.java
+++ src/main/java/com/impact/core/module/assetRequest/entity/AssetRequest.java
@@ -1,17 +1,21 @@
-package com.impact.core.entities;
+package com.impact.core.module.assetRequest.entity;
 
 import com.impact.core.module.asset.entity.Asset;
 import com.impact.core.module.resource_request_status.entity.ResourceRequestStatus;
+import com.impact.core.module.user.entity.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
+import java.time.Instant;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "asset_request")
+@Table(name = "asset_petition")
 public class AssetRequest {
     @Id
     @Column(name = "id", nullable = false)
@@ -31,5 +35,14 @@ public class AssetRequest {
 
     @Column(name = "expiration_date")
     private LocalDate expirationDate;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
 
 }
