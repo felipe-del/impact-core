40054a1ee3d76c165c8d703be7f474f30ed0ec0b
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-08 03:06:55 -0600

    Rename AssetRequest to AssetPetition and update entity.
    Renamed the AssetRequest entity to AssetPetition for clearer domain representation. Added Lombok annotations for constructors and a builder, replaced `Instant` with `LocalDateTime` for `createdAt`, and implemented a `@PrePersist` method to initialize timestamps. Updated the table name and applied other minor improvements.
    
    Took 1 hour 35 minutes

--- src/main/java/com/impact/core/module/assetRequest/entity/AssetPetition.java
+++ src/main/java/com/impact/core/module/assetRequest/entity/AssetPetition.java
@@ -5,20 +5,24 @@ import com.impact.core.module.resource_request_status.entity.ResourceRequestStat
 import com.impact.core.module.user.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "asset_petition")
-public class AssetRequest {
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssetPetition {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -41,8 +45,14 @@ public class AssetRequest {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ColumnDefault("CURRENT_TIMESTAMP")
     @Column(name = "created_at")
-    private Instant createdAt;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
 
 }
