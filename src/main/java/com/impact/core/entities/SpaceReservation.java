bbe636144fe8b29f1158bff23630b0dca882a95f
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-12 19:36:55 -0600

    Add SpaceReservation entity class
    Introduce the `SpaceReservation` entity to represent space reservations with attributes for `id`, `space`, `startTime`, and `endTime`. This class maps to the `space_reservation` database table and includes relationships with the `Space` entity.
    
    Took 7 seconds

---
+++ src/main/java/com/impact/core/entities/SpaceReservation.java
@@ -0,0 +1,30 @@
+package com.impact.core.entities;
+
+import com.impact.core.module.space.entity.Space;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "space_reservation")
+public class SpaceReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "space_id")
+    private Space space;
+
+    @Column(name = "start_time")
+    private Instant startTime;
+
+    @Column(name = "end_time")
+    private Instant endTime;
+
+}
