4922217368e7200852d44db1d5d8e6219a86506a
JoF00L <131736806+JoF00L@users.noreply.github.com>
2025-03-03 15:02:50 -0600

    feat (spaceRndRController): Added the controller of handling petitions involving objects of either SpaceRequest or SpaceReservation.

---
+++ src/main/java/com/impact/core/module/spaceRequest_Reservation/controller/SpaceRndRController.java
@@ -0,0 +1,12 @@
+package com.impact.core.module.spaceRequest_Reservation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/space-request&reservation")
+@RequiredArgsConstructor
+public class SpaceRndRController {
+    // IN DEVELOPMENT
+}
