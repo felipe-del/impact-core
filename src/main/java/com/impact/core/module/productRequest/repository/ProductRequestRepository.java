e8965297b98c17b1c67b7bacd53ed48bc8c0a896
Raquel <raquealfaba@gmail.com>
2025-03-02 18:38:50 -0600

    feat(repository): add query to fetch product requests by user
    Added a custom query in ProductRequestRepository to retrieve product requests filtered by user ID.

--- src/main/java/com/impact/core/module/productRequest/repository/ProductRequestRepository.java
+++ src/main/java/com/impact/core/module/productRequest/repository/ProductRequestRepository.java
@@ -1,9 +1,16 @@
 package com.impact.core.module.productRequest.repository;
 
+import com.impact.core.module.assetRequest.entity.AssetRequest;
 import com.impact.core.module.productRequest.entity.ProductRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRequestRepository extends JpaRepository<ProductRequest, Integer> {
+    @Query("SELECT p FROM ProductRequest p WHERE p.user = :userId")
+    List<ProductRequest> productsRequestByUser(@Param("userId") Integer userId);
 }
