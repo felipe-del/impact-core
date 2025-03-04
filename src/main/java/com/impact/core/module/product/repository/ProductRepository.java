fa53d92b665ee653730c39d47f3a7936527b0d4e
felipe-del <isaacfelibrenes1904@gmail.com>
2025-03-04 11:22:52 -0600

    Add query to fetch available products by category
    Introduced a method in ProductRepository to retrieve available products filtered by category name. This enhances the repository's functionality to support more targeted queries for active products.

--- src/main/java/com/impact/core/module/product/repository/ProductRepository.java
+++ src/main/java/com/impact/core/module/product/repository/ProductRepository.java
@@ -2,8 +2,14 @@ package com.impact.core.module.product.repository;
 
 import com.impact.core.module.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("SELECT p FROM Product p WHERE p.category.name = :categoryName AND p.status.id = 1")
+    List<Product> findAvailableProductsByCategory(@Param("categoryName") String categoryName);
 }
