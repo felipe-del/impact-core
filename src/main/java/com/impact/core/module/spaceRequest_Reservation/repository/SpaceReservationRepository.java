cb4c9eb3c0407418d532bf4c3e1673c1da4c841c
felipe-del <isaacfelibrenes1904@gmail.com>
2025-03-05 02:48:17 -0600

    Add method to fetch reservations by space
    Introduce a new query method `findAllBySpace` in `SpaceReservationRepository` to retrieve all reservations associated with a specific space. This enhances data retrieval functionality and simplifies related queries.

--- src/main/java/com/impact/core/module/spaceRequest_Reservation/repository/SpaceReservationRepository.java
+++ src/main/java/com/impact/core/module/spaceRequest_Reservation/repository/SpaceReservationRepository.java
@@ -1,9 +1,13 @@
 package com.impact.core.module.spaceRequest_Reservation.repository;
 
+import com.impact.core.module.space.entity.Space;
 import com.impact.core.module.spaceRequest_Reservation.entity.SpaceReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpaceReservationRepository extends JpaRepository<SpaceReservation, Integer> {
+    List<SpaceReservation> findAllBySpace(Space space);
 }
