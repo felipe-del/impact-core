d7475dc60de03204799f8df5307084897b640129
felipe-del <isaacfelibrenes1904@gmail.com>
2025-03-21 09:53:00 -0600

    Fix package naming convention in ProductsOfRequestRepository
    Updated package names to follow standard Java naming conventions by switching to lowercase letters. This ensures consistency across the codebase and adheres to best practices.

--- src/main/java/com/impact/core/module/productsOfRequest/repository/ProductsOfRequestRepository.java
+++ src/main/java/com/impact/core/module/productsOfRequest/repository/ProductsOfRequestRepository.java
@@ -1,14 +1,14 @@
-package com.impact.core.module.ProductsOfRequest.repository;
+package com.impact.core.module.productsOfRequest.repository;
 
 
-import com.impact.core.module.ProductsOfRequest.entity.ProductsOfRequest;
+import com.impact.core.module.productsOfRequest.entity.ProductsOfRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProductsOfRequestRepository extends JpaRepository<com.impact.core.module.ProductsOfRequest.entity.ProductsOfRequest, Integer> {
+public interface ProductsOfRequestRepository extends JpaRepository<com.impact.core.module.productsOfRequest.entity.ProductsOfRequest, Integer> {
     @Query("SELECT p FROM ProductsOfRequest p WHERE p.productRequest.id = :requestId")
     List<ProductsOfRequest> productsOfRequestByR(@Param("requestId") Integer requestId);
 }
