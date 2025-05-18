7b13f1a8d6b331609a179ce72d2f553fd5217330
JoF00L <131736806+JoF00L@users.noreply.github.com>
2025-05-17 18:06:42 -0600

    refactor(request-service): refactoring of an import and variable related to the SpaceRequestAndReservationService class.

--- src/main/java/com/impact/core/module/request/service/RequestService.java
+++ src/main/java/com/impact/core/module/request/service/RequestService.java
@@ -1,16 +1,13 @@
 package com.impact.core.module.request.service;
 
 import com.impact.core.module.assetRequest.entity.AssetRequest;
-import com.impact.core.module.assetRequest.repository.AssetRequestRepository;
 import com.impact.core.module.assetRequest.service.AssetRequestService;
 import com.impact.core.module.productRequest.entity.ProductRequest;
-import com.impact.core.module.productRequest.repository.ProductRequestRepository;
 import com.impact.core.module.productRequest.service.ProductRequestService;
 import com.impact.core.module.request.mapper.RequestMapper;
 import com.impact.core.module.request.payload.RequestDTOResponse;
 import com.impact.core.module.spaceRequest_Reservation.entity.SpaceRequest;
-import com.impact.core.module.spaceRequest_Reservation.repository.SpaceRequestRepository;
-import com.impact.core.module.spaceRequest_Reservation.service.SpaceRndRService;
+import com.impact.core.module.spaceRequest_Reservation.service.SpaceRequestAndReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +26,7 @@ public class RequestService {
 
     private final AssetRequestService assetRequestService;
     private final ProductRequestService productRequestService;
-    private final SpaceRndRService spaceRequestService;
+    private final SpaceRequestAndReservationService spaceRequestService;
     private final RequestMapper requestMapper;
 
     /**
